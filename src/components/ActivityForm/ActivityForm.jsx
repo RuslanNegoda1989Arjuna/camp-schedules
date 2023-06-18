11ff86b928fd5f3935b2734fb5719b4528be69f5
Ruslan Negoda <103960102+RuslanNegoda1989Arjuna@users.noreply.github.com>
2023-06-18 23:46:17 +0300

    add smile

--- src/components/ActivityForm/ActivityForm.jsx
+++ src/components/ActivityForm/ActivityForm.jsx
@@ -1,9 +1,11 @@
-import ButtonSkew from 'components/ButtonSkew/ButtonSkew';
 import React, { useState } from 'react';
-import styles from './ActivityForm.module.scss'
+import styles from './ActivityForm.module.scss';
+import EmojiPicker from '../EmojiPicker/EmojiPicker';
+import ButtonSkew from 'components/ButtonSkew/ButtonSkew';
 
 const ActivityForm = ({ onAddOption }) => {
   const [activity, setActivity] = useState('');
+  const [showEmojiPicker, setShowEmojiPicker] = useState(false);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -18,13 +20,25 @@ const ActivityForm = ({ onAddOption }) => {
     setActivity(e.target.value);
   };
 
+  const handleEmojiClick = (emoji) => {
+    setActivity((prevActivity) => prevActivity + emoji);
+  };
+
+  const toggleEmojiPicker = () => {
+    setShowEmojiPicker((prevShowEmojiPicker) => !prevShowEmojiPicker);
+  };
+
   return (
     <form className={styles.formBox} onSubmit={handleSubmit}>
       <label className={styles.labelActivity}>
         Нова діяльність:
         <input className={styles.inputActivity} type="text" value={activity} onChange={handleChange} />
-          </label>
-          <ButtonSkew type="submit" text="Додати"  />
+      </label>
+      <button className={styles.smileBtn} type="button" onClick={toggleEmojiPicker}>
+        {showEmojiPicker ? '😝' : '😃'}
+      </button>
+      {showEmojiPicker && <EmojiPicker onEmojiClick={handleEmojiClick} />}
+      <ButtonSkew type="submit" text="Додати" />
     </form>
   );
 };
